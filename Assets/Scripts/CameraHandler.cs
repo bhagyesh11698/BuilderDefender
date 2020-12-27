a6dfe686406ce9a9f9a1222c2aeb3c1e5a755278
Bhagyesh Patel <11698guru@gmail.com>
2020-12-27 22:51:42 +0530

    EP 14 Cinemachine
    - Created World Map
    - Created Camera that moves and zooms in / out using Cinemachine
    - added layers

---
+++ Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+
+
+public class CameraHandler : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+    private float orthographicSize;
+    private float targerOrthographicSize;
+
+    private void Start()
+    {
+        orthographicSize = cinemachineVirtualCamera.m_Lens.OrthographicSize;
+        targerOrthographicSize = orthographicSize;
+
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleZoom();
+
+    }
+
+    void HandleMovement()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        // normalized will make sure that it does not move too fast
+        Vector2 moveDir = new Vector2(x, y).normalized;
+        float moveSpeed = 5f;
+
+        //-- Make Movement
+        transform.position += (Vector3)moveDir * moveSpeed * Time.deltaTime;
+        //either make it vector 3 or change moveDir to vector 3
+    }
+
+    void HandleZoom()
+    {
+        //-- Zoom in / out
+        float zoomAmount = 2f;
+        // - will invert mouse scrolling functionality
+        targerOrthographicSize += -Input.mouseScrollDelta.y * zoomAmount;
+
+        float minOrthographicSize = 10;
+        float maxOrthographicSize = 30;
+        targerOrthographicSize = Mathf.Clamp(targerOrthographicSize, minOrthographicSize, maxOrthographicSize);
+
+        float zoomSpeed = 5f;
+        orthographicSize = Mathf.Lerp(orthographicSize, targerOrthographicSize, Time.deltaTime);
+
+        cinemachineVirtualCamera.m_Lens.OrthographicSize = orthographicSize;
+    }
+
+
+}
